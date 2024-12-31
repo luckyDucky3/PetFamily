7c21fe959613d7daed08f6db8ce7c0b643b37da3
luckyDucky3 <maxlebed04@mail.ru>
2024-12-31 19:29:08 +0300

    подключение БД через докерфайл, добавление ApplicationDbContext, секретов и LoggerFactory

---
+++ PetFamily.Backend/src/PetFamily.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Immutable;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using PetFamily.Domain.Models;
+
+namespace PetFamily.Infrastructure;
+
+public class ApplicatonDbContext(IConfiguration configuration) : DbContext
+{
+    private const string DATABASE = "Database";
+    DbSet<Pet> Pets => Set<Pet>();
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(configuration.GetConnectionString(DATABASE));
+        optionsBuilder.UseSnakeCaseNamingConvention();
+        optionsBuilder.UseLoggerFactory(CreateLoggerFactory());
+    }
+
+    private ILoggerFactory CreateLoggerFactory() =>
+        LoggerFactory.Create(builder => { builder.AddConsole(); });
+}
